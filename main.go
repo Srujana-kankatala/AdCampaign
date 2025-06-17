d1d31c34def2a69e0e6949fe06d1cd3b0b97353d
Srujana-kankatala <srujana140598@gmail.com>
2025-06-17 15:15:00 +0530

    added prometheus functionality

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"AdCampaign/util"
 	"AdCampaign/db"
 	"github.com/gin-gonic/gin"
+	prometheus "github.com/zsais/go-gin-prometheus"
 )
 
 func main(){
 
 	db.InitDB()
 	r := gin.Default()
+
+	p := prometheus.NewPrometheus("ad_campaign_service")
+	p.Use(r)
+
 	r.GET("/v1/delivery",util.DeliveryService)
 	r.Run("localhost:8080")
 }
